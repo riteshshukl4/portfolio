160213c435461d51417bc3d68b1f0fd2298f8b94
Ritesh Shukla <riteshshuklamail@gmail.com>
2024-12-06 15:54:56 +0530

    make changes to hero.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { ThemeProvider } from "@/components/theme-provider";
 
 const inter = Inter({ subsets:["latin"] });
 
@@ -19,7 +20,13 @@ export default function RootLayout({
       <body
         className={inter.className}
       >
-        {children}
+        <ThemeProvider
+            attribute="class"
+            defaultTheme="dark"
+            enableSystem
+            disableTransitionOnChange>
+            {children}
+          </ThemeProvider>
       </body>
     </html>
   );
